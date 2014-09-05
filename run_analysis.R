17a2e3a6270e0b7eef51029851d1670abb823bef
gordonshamway <stef.buchholz@googlemail.com>
2014-09-05 16:00:51 +0200

    Added correct Unzip stuff and build up step 5 to write down the tidy dataset to submit

--- run_analysis.R
+++ run_analysis.R
@@ -1,70 +1,79 @@
-#download the data
-#Windows specific workaround
+#Download the data
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(url,destfile="file.zip", method="curl")
+#download.file(url,destfile="file.zip", method="curl") #for mac
+download.file(url,destfile="file.zip") #for windows
 
 #extract the file in the working directory
-unzip("file.zip",list=TRUE, exdir = temp, unzip="internal")
+unzip("file.zip",list=FALSE, unzip="internal")
 
 #read in common stuff
 ########################
 #read in activity labels
-activity_labels <- read.table("/Users/stefan/getting_and_cleaning_data/UCI HAR Dataset/activity_labels.txt")
+activity_labels <- read.table(paste(getwd(),"/UCI HAR Dataset/activity_labels.txt", sep=""))
 
 #read in column names
-features <- read.table("/Users/stefan/getting_and_cleaning_data/UCI HAR Dataset/features.txt")
+features <- read.table(paste(getwd(),"/UCI HAR Dataset/features.txt", sep=""))
 
 #Read in Training-Stuff
 #########################
 #read in subjects
-train_subjects <- read.table("/Users/stefan/getting_and_cleaning_data/UCI HAR Dataset/train/subject_test.txt")
+train_subjects <- read.table(paste(getwd(),"/UCI HAR Dataset/train/subject_train.txt", sep=""))
 
 #read in data
-train <- read.table("/Users/stefan/getting_and_cleaning_data/UCI HAR Dataset/train/X_train.txt")
+train <- read.table(paste(getwd(),"/UCI HAR Dataset/train/X_train.txt", sep=""))
 
 #read in results
-train_results <- read.table("/Users/stefan/getting_and_cleaning_data/UCI HAR Dataset/train/y_train.txt")
+train_results <- read.table(paste(getwd(),"/UCI HAR Dataset/train/y_train.txt", sep=""))
 
 
 #Read in Test-Stuff
 #########################
 #read in subjects
-test_subjects <- read.table("/Users/stefan/getting_and_cleaning_data/UCI HAR Dataset/test/subject_test.txt")
+test_subjects <- read.table(paste(getwd(),"/UCI HAR Dataset/test/subject_test.txt", sep=""))
 
 #read in data
-test <- read.table("/Users/stefan/getting_and_cleaning_data/UCI HAR Dataset/test/X_test.txt")
+test <- read.table(paste(getwd(),"/UCI HAR Dataset/test/X_test.txt",sep=""))
 
 #read in results
-test_results <- read.table("/Users/stefan/getting_and_cleaning_data/UCI HAR Dataset/test/y_test.txt")
+test_results <- read.table(paste(getwd(),"/UCI HAR Dataset/test/y_test.txt", sep=""))
 
+
+#Step 1: Merge the datasets
+###############################
 #setting colnames of the data
 colnames(test) <- features$V2
-colnames(training) <- features$V2
+colnames(train) <- features$V2
 
 #build up whole test dataset
 test_frame <- data.frame(test_subjects, test, test_results)
 train_frame <- data.frame(train_subjects, train, train_results)
-frame <- data.frame(train_frame, test_frame)
+frame <- rbind(test_frame,train_frame)
 
 #make up good column names
 colnames(frame)[1] <- "subject"
 colnames(frame)[ncol(frame)] <- "id"
 
+# Step 3: Map to better activity names
+#########################################
 #join real activity labels with activity numbers
 library(plyr)
 colnames(activity_labels) <- c("id","activity")
 frame <- arrange(join(frame, activity_labels), id)
-frame <- test_set[-563]
+frame <- frame[-563]
+
+# Step 2: Keeping specific measurements
+############################################
 
 #just keep the std and mean columns
-means <- grep("mean" ,colnames(frame),value=TRUE)
-stds <- means <- grep("std" ,colnames(frame),value=TRUE)
-variable_filter <- c(means,stds)
+means <- grep("mean" ,colnames(frame),value=TRUE, ignore.case = TRUE)
+stds <- grep("std" ,colnames(frame),value=TRUE, ignore.case = TRUE)
+variable_filter <- c(means, stds, "activity", "subject")
 
 #I have to filter the combined test and training dataset on those variables
-keeped <- frame[,(names(frame) %in% variable_filter)]
+keeped <- frame[,(names(frame) %in% variable_filter)] #There should be 88 columns
 
-#clean the colum names
+#Step 4: clean the colum names
+#####################################
 names(keeped) <- gsub("\\.","", names(keeped),)
 names(keeped) <- gsub("Acc","Accelerometer-", names(keeped),)
 names(keeped) <- gsub("Gyro","Gyroscope-", names(keeped),)
@@ -76,3 +85,12 @@ names(keeped) <- gsub("^f","frequency-", names(keeped),)
 names(keeped) <- gsub("jerk","jerk-", names(keeped),)
 names(keeped) <- tolower(names(keeped))
 names(keeped) <- gsub("mag","mag-", names(keeped),)
+names(keeped) <- gsub("^angle","angle-", names(keeped),)
+
+# Step 5: Build small tidy dataset with avg of every variable by subject and activity
+tidy_dataset <- aggregate(keeped, by=list(keeped$subject, keeped$activity), mean)
+tidy_dataset$subject <- NULL
+tidy_dataset$activity <- NULL
+names(tidy_dataset)[1] <- "subject"
+names(tidy_dataset)[2] <- "activity"
+write.table(tidy_dataset, row.name=FALSE, file="tidy_dataset.txt")
